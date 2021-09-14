1f275fc031db7061c73e29cc6d191bc1c48f9776
Y Tran <40287088+ytranhn@users.noreply.github.com>
2021-09-14 10:20:52 +0700

    Confirm delete member (#45)
    * relsove conflict
    
    * relsove conflict
    
    * resolve conflict
    
    * merge conflict
    
    * relsove conflict
    
    * fix conflict
    
    * check
    
    * css check
    
    * check component
    
    * service
    
    * update group
    
    * update confirm delete member

--- src/app/modules/group/group-detail/group-detail.component.ts
+++ src/app/modules/group/group-detail/group-detail.component.ts
@@ -10,6 +10,7 @@ import { UpdateNameComponent } from './update-name/update-name.component';
 import { UpdatePhoneComponent } from './update-phone/update-phone.component';
 import { UpdateSupportComponent } from './update-support/update-support.component';
 import { S3Service } from "../../../core/services/s3.service";
+import { DeleteMemberComponent } from './delete-member/delete-member.component';
 
 @Component({
   selector: 'app-group-detail',
@@ -128,20 +129,17 @@ export class GroupDetailComponent implements OnInit {
   }
 
   removeMember(groupId: any, memberId: any) {
-    let data = {
-      "members": [
-        {
-          "id": memberId,
-        },
-      ],
-    };
-    this.GroupService.removeMemberGroup(groupId, data).subscribe((data: any) => {
-      if(data){
-        this.notification.success("Xoá thành công");
-        this.group.members = data.data.members;
-        return;
+    this.dialog.open(DeleteMemberComponent, {
+      panelClass: 'deleteMember',
+      disableClose: true,
+      data: {
+        memberId: memberId,
+        id: groupId,
+      },
+    }).afterClosed().subscribe((result: any)=>{
+      if(result){
+        this.group.members = result.data.data.members;
       }
-      this.notification.error("Xoá thất bại");
     })
   }
 
